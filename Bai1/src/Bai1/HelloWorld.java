28792fd51a96b3221e8a0a03b76b920929e18eae
chiuthuyty <thuyty1411@gmail.com>
2024-10-10 10:42:10 +0700

    10102024

---
+++ Bai1/src/Bai1/HelloWorld.java
@@ -0,0 +1,8 @@
+package Bai1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with JAVA");
+		System.out.println("Hello World with JAVA");
+	}
+}
